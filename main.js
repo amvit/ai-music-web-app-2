5d842864a4cac3c91f9759e0f81a04ca86597bcd
amvit <73838043+amvit@users.noreply.github.com>
2022-06-01 19:49:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 song1="";
 song2="";
+leftWristX= 0;
+leftWristY= 0;
+rightWristX= 0;
+rightWristY= 0;
+
 
 function preload(){
     song1= loadSound("music.mp3");
@@ -11,6 +16,24 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = "+ leftWristX +" leftWristY = "+ leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+ rightWristX +" rightWristY = "+ rightWristY);
+    }
+}
+
+function modelLoaded(){
+    console.log('Posenet is initialized');
 }
 
 function draw(){
@@ -19,4 +42,6 @@ function draw(){
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
