c153ebae8dac70085350f6d05df5a5c9f058cb00
amvit <73838043+amvit@users.noreply.github.com>
2022-06-02 18:56:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+song1_status ="";
+song2_status ="";
+scoreLeftWrist = "";
+scoreRightWrist ="";
 leftWristX= 0;
 leftWristY= 0;
 rightWristX= 0;
@@ -23,22 +27,51 @@ function setup(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = "+ leftWristX +" leftWristY = "+ leftWristY);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].scotre;
+        console.log("scoreRightWrist =" + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
-        console.log("rightWristX = "+ rightWristX +" rightWristY = "+ rightWristY);
+        console.log("rightWristX =" + rightWristX + "rightWristY = " + rightWristY);
+
+        leftWristX = results[0].pose.rightWrist.x;
+        leftWristY = results[0].pose.rightWrist.y;
+        console.log("leftWristX =" + leftWristX + "leftWristY = " + leftWristY);
     }
 }
+
+
 
 function modelLoaded(){
     console.log('Posenet is initialized');
 }
 
-function draw(){
+function draw()
     image(video, 0, 0, 600, 500);
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+    fill("#FF0000")
+    stroke("#FF0000")
+    if(scoreRightWrist > 0.2){
+        circle(rightWristX,rightWristY,20);
+        song2.stop();
+        if(song1_status == false){
+            song1.play();
+            document.getElementById("song").innerHTML ="playing - Harry potter theme song"
+        }
+
+    }
+
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX,leftWristY,20);
+        song1.stop();
+        if(song2_status == false){
+            song2.play();
+            document.getElementById("song").innerHTML ="playing - Peter Pan song"
+        }
 }
+
 
 function play(){
     song.play();
